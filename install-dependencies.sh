a02aad5492f0006302ddfdd742d64e13b342f823
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-10-22 16:47:28 -0700

    install-dependencies.sh: set GCC version to 15 and LLVM to 21 (#89)
    Currently we cross-compile aarch64 and s390x, so installing build
    dependencies and Linux mirror repo only makes sense for x86_64.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,21 +3,25 @@
 set -euo pipefail
 
 # Most of the BPF CI dependencies are set up by libbpf/ci/setup-build-env action
-# This script runs a subset of it, in order to cache the packages at image build time
+# This script runs a subset of that, in order to cache the packages at image build time
 export LIBBPF_CI_TAG=v3
 
 # These should correspond to https://github.com/kernel-patches/vmtest/blob/master/.github/scripts/matrix.py#L20-L21
 # Otherwise there is no point in caching dependencies in the image
-export GCC_VERSION=14
-export LLVM_VERSION=20
+export GCC_VERSION=15
+export LLVM_VERSION=21
+
+# do not install pahole and cross-compilation toolchain in the docker image
+export TARGET_ARCH=$(uname -m)
+export PAHOLE_BRANCH=none
 
 scratch=$(mktemp -d)
 cd $scratch
-
 git clone --depth 1 --branch $LIBBPF_CI_TAG https://github.com/libbpf/ci.git actions
-cd actions
-./setup-build-env/install_packages.sh
-./setup-build-env/install_clang.sh
-./run-vmtest/install-dependencies.sh
 
-# do not install pahole and cross-compilation toolchain in the docker image
+# Install build dependencies only on x86_64, we cross-compile everything else
+if [ "$(uname -m)" == "x86_64" ]; then
+        cd "${scratch}/actions/setup-build-env" && ./action.sh
+fi
+
+cd "${scratch}/actions/run-vmtest" && ./install-dependencies.sh
--- setup-mirror-repos.sh
+++ setup-mirror-repos.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if [ "$(uname -m)" != "x86_64" ]; then
+        echo "Skip setting up mirror repos in non-x86_64 image"
+        exit 0
+fi
+
 mkdir -p /libbpfci/mirrors
 git clone https://github.com/kernel-patches/bpf.git /libbpfci/mirrors/linux
 chmod -R a+rX /libbpfci/mirrors
